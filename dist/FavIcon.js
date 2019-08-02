8ed3e110597c2ea4cded0325c49249a2f671559f
Jay Harris <jay.harris@outlook.co.nz>
2019-08-02 10:20:24 +1000

    Begins working on favicon

--- dist/FavIcon.js
+++ dist/FavIcon.js
@@ -1,39 +1,29 @@
-const getLinkHtml = (href: string) => `<link rel="shortcut icon" href="${href}"`;
-
+const getLinkHtml = (href) => `<link rel="shortcut icon" href="${href}"`;
 class FavIcon extends HTMLElement {
     static get observedAttributes() {
-        return [ "href" ]
-    };
-
-    private link: HTMLLinkElement;
-
+        return ["href"];
+    }
+    ;
     constructor() {
         super();
-
         this.link = document.createElement('link');
         this.link.setAttribute("rel", "shortcut icon");
-
         document.head.appendChild(this.link);
-
         this.update();
     }
-
     get href() {
         return this.getAttribute('href');
     }
-
     set href(value) {
         this.setAttribute('href', value);
     }
-
     attributeChangedCallback(name, oldValue, newValue) {
-        if (name !== "href" || oldValue == newValue) return;
+        if (name !== "href" || oldValue == newValue)
+            return;
         this.update();
     }
-    
-    private update() {
+    update() {
         this.link.setAttribute("href", this.href);
     }
 }
-
 customElements.define('fav-icon', FavIcon);
